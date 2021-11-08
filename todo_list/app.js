38c6afda56766419cb7d5888e97bfd6170fb4d2c
Candycrush69 <90060383+Candycrush69@users.noreply.github.com>
2021-11-08 13:50:41 +0300

    Add files via upload

---
+++ todo_list/app.js
@@ -0,0 +1,114 @@
+const input = document.querySelector('#todo__input')
+const btn = document.querySelector('#todo__add')
+const list = document.querySelector('#todo__list')
+
+btn.addEventListener('click', () => {
+  const value = input.value
+  if (value =='') {alert('Поле ввода не должно быть пустым! Повторите попытку снова.')} 
+  else {
+    sendName(value)
+    input.value = ''
+    }
+  
+})
+
+fetchTodo()
+  
+list.addEventListener('click', e => {
+  const id = e.target.closest('li')?.dataset.id
+  const tag = e.target.tagName
+  const type = e.target.type
+  const checked = e.target.checked;
+  const todo = e.target.closest('li')
+
+  if (tag === 'INPUT' && type === 'checkbox') {
+    updateTodo(todo)
+  }
+
+  if (tag === 'BUTTON') {
+    var element = document.querySelector('input[type=checkbox]');   
+      if (element.checked) {
+        e.target.closest('li').remove()
+        removeTodo(id)
+      } else {alert('Вы не можете удалить невыполненный пункт.')} 
+      
+  } else if (tag === 'SPAN') {
+    const target = e.target
+    replaceOnInput(target)
+  }
+})
+
+function fetchTodo() {
+  fetch('./php/todo.php')
+    .then(res => res.json())
+    .then(res => {
+      list.innerHTML = ''
+      res?.forEach(todo => {
+        list.innerHTML += `
+          <li class="todo__item" data-id='${todo.id}'>
+            <input type="checkbox" ${todo.checked == true ? 'checked' : ''} class="todo__check">
+            <span>${todo.text}</span>
+            <button class="todo__remove">&times;</button>
+          </li>
+        `
+      })
+    })
+}
+
+function sendName(todo) {
+  fetch('./php/todo.php', {
+    method: 'POST',
+    body: JSON.stringify({
+      todo: todo
+    })
+  }).then(res => fetchTodo())
+}
+
+function updateTodo(todo) {
+  const id = todo.dataset.id
+  const checked = todo.querySelector('input').checked
+  const text = todo.querySelector('span').textContent
+
+  fetch('./php/todo.php', {
+    method: 'PUT',
+    body: JSON.stringify({
+      id: id,
+      checked: checked,
+      text: text
+    })
+  })
+}
+
+function removeTodo(id) {
+  console.log(id);
+  fetch('./php/todo.php', {
+    method: 'DELETE',
+    body: JSON.stringify({
+      id: id,
+    })
+  })
+}
+
+function replaceOnInput(target) {
+  const todo = target.closest('li')
+  const input = document.createElement('input')
+  input.value = target.textContent
+  target.replaceWith(input)
+  
+  // Прослушиватели
+  input.focus()
+  input.addEventListener('blur', () => {
+    replaceOnSpan(input)
+    updateTodo(todo)
+  })
+}
+
+function replaceOnSpan(target) {
+  target.insertAdjacentHTML('afterend', 
+    `<span>${target.value}</span>`
+  )
+  target.remove()
+}
+
+
+
---
+++ todo_list/php/todo.php
@@ -0,0 +1,43 @@
+<?php
+  require '../php/db.php';
+
+  $link = mysqli_connect($host, $user, $password, $db) or die("Ошибка " . mysqli_error($link));
+
+  if ($_SERVER['REQUEST_METHOD'] === 'GET') 
+  {
+    $sql = "SELECT * FROM todo_list;";
+    $result = $link->query($sql);
+
+    $todos = array();
+    while($row = $result->fetch_assoc()) {
+      $todo[] = $row;
+    }
+    echo json_encode($todo);
+  }
+  else if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+  {
+    $_POST = json_decode(file_get_contents('php://input'), true);  
+    $todo = $_POST['todo'];
+    
+    $sql = "INSERT INTO todo_list (text) VALUES('$todo')";
+    $result = $link->query($sql);
+  }
+  else if ($_SERVER['REQUEST_METHOD'] === 'PUT') 
+  {
+    $_PUT = json_decode(file_get_contents('php://input'), true);  
+    $id = $_PUT['id'];
+    $boolean = $_PUT['checked'] ? 1 : 0;
+    $text = $_PUT['text'];
+    $sql = "UPDATE `todo_list` SET `checked`='$boolean', `text`='$text' WHERE id = $id";
+    var_dump($sql);
+    $result = $link->query($sql);
+  }
+  else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') 
+  {
+    $_POST = json_decode(file_get_contents('php://input'), true);  
+    $id = $_POST['id'];
+    $sql = "DELETE FROM `todo_list` WHERE id=$id";
+    $result = $link->query($sql);
+  }
+  
+?>
